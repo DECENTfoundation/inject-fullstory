a84f24ac118a9c45299530dd81ecc039a86f4509
Robert <robert.simon@decent.ch>
2020-01-14 14:57:15 +0100

    web usage

--- index.js
+++ index.js
@@ -1,64 +1,23 @@
-var through = require('through2');
-
-module.exports = function(opts) {
-
-	opts = opts || {};
-	opts.id = opts.id || '';
-	opts.namespace = opts.namespace || 'FullStory namespace conflict. Please set window["_fs_namespace"].';
-	opts.tag = opts.tag || 'head';
-
-
-	return through.obj(function(file, enc, cb) {
-
-		if(file.isNull()) return cb(null, file);
-		if(file.isStream()) return cb(new Error("Custom plugin: streams not supported."));
-
-
-		////////////////////////////////////////////////////////////////////////////////////////////////////
-
-		var fullstoryScript = "<script>\n"+
-
-				"window['_fs_debug'] = false;\n"+
-
-				"window['_fs_host'] = 'fullstory.com';\n"+
-
-				"window['_fs_org'] = '"+opts.id+"';\n"+
-
-				"window['_fs_namespace'] = 'FS';\n"+
-
-				"(function(m,n,e,t,l,o,g,y) {\n"+
-
-				"if (e in m) {if(m.console && m.console.log) {m.console.log('"+opts.namespace+"');} else {return;}}\n"+
-
-				"g=m[e]=function(a,b){g.q?g.q.push([a,b]):g._api(a,b);};g.q=[];\n"+
-
-				"o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';\n"+
-
-				"y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);\n"+
-
-				"g.identify=function(i,v){g(l,{uid:i});if(v)g(l,v)};g.setUserVars=function(v){g(l,v)};g.event=function(i,v){g('event',{n:i,p:v})};\n"+
-
-				'g.shutdown=function(){g("rec",!1)};g.restart=function(){g("rec",!0)};\n'+
-
-				'g.consent=function(a){g("consent",!arguments.length||a)};\n'+
-
-				"g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};\n"+
-
-				"g.clearUserCookie=function(){};\n"+
-
-				"})(window,document,window['_fs_namespace'],'script','user');";
-
-				//"</script>"
-
-			fullstoryScript += "</script>\n  </"+opts.tag+">\n";
-
-		////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-		var content = file.contents.toString()
-		content = content.replace('<\/' + opts.tag + '>', fullstoryScript);
-		file.contents = new Buffer(content)
-		cb(null, file)
-	})
-
+const FullStory = {
+	initialize: function(org) {
+		const _fs_script = 'edge.fullstory.com/s/fs.js';
+		window['_fs_debug'] = false;
+		window['_fs_host'] = 'fullstory.com';
+		window['_fs_org'] = org;
+		window['_fs_namespace'] = 'FS';
+		(function (m, n, e, t, l, o, g, y) {
+			if (e in m) { if (m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].'); } return; }
+			g = m[e] = function (a, b, s) { g.q ? g.q.push([a, b, s]) : g._api(a, b, s); }; g.q = [];
+			o = n.createElement(t); o.async = 1; o.crossOrigin = 'anonymous'; o.src = 'https://' + _fs_script;
+			y = n.getElementsByTagName(t)[0]; y.parentNode.insertBefore(o, y);
+			g.identify = function (i, v, s) { g(l, { uid: i }, s); if (v) g(l, v, s) }; g.setUserVars = function (v, s) { g(l, v, s) }; g.event = function (i, v, s) { g('event', { n: i, p: v }, s) };
+			g.shutdown = function () { g("rec", !1) }; g.restart = function () { g("rec", !0) };
+			g.log = function (a, b) { g("log", [a, b]) };
+			g.consent = function (a) { g("consent", !arguments.length || a) };
+			g.identifyAccount = function (i, v) { o = 'account'; v = v || {}; v.acctId = i; g(o, v) };
+			g.clearUserCookie = function () { };
+		})(window, document, window['_fs_namespace'], 'script', 'user');
+	}
 };
+
+module.exports = FullStory;
